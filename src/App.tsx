b224568ef3e434e9e25f42990d8db4f669382847
Azim Gadzhiagayev <me@azimgd.com>
2021-10-12 18:38:32 +0500

    Typescript absolute imports support

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
-import ModalTemplate from './templates/Modal'
-import PaymentsComponent from './components/Payments'
-import theme from './theme'
+import ModalTemplate from 'templates/Modal'
+import PaymentsComponent from 'components/Payments'
+import theme from 'theme'
 
 function App() {
   return (
