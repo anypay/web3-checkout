bfcb22ae999ff3bbdd39187b7420be3b598b9cc9
Azim Gadzhiagayev <me@azimgd.com>
2021-10-30 12:02:43 +0500

    config and setup tx

--- src/services/Anypay/__tests__/index.ts
+++ src/services/Anypay/__tests__/index.ts
@@ -1,8 +1,27 @@
 import AnypayService from 'services/Anypay'
 import { renderHook, act } from '@testing-library/react-hooks'
+import * as bsv from 'bsv'
 
 describe('AnypayService', () => {
   const anypay = renderHook(() => AnypayService())
+  const outputs = [{ satoshis: 1000, script: '11122223333' }]
+  const inputs = [{
+    address: '1E9URzUqKRgYSV8hHtfgm6WAB9bxhLJos5',
+    txid: 'a02d809b43e279742bf9fc847531b389b0a1728a46ad05a3261cd8c71b1ae86e',
+    vout: 0,
+    amount: 0.0013,
+    satoshis: 130000,
+    value: 130000,
+    height: 711028,
+    confirmations: 271,
+    scriptPubKey: '76a9149033dc6c621604f61f70bada8d9adbdc1105806888ac',
+    script: '76a9149033dc6c621604f61f70bada8d9adbdc1105806888ac',
+    outputIndex: 0
+  }]
+  
+  const changeTo = '1E9URzUqKRgYSV8hHtfgm6WAB9bxhLJos5'
+  const privateKey = bsv.PrivKey.Mainnet.fromRandom()
+  const keyPair = bsv.KeyPair.fromPrivKey(privateKey)
 
   test('AnypayService#configure', () => {
     expect(anypay.result.current.getState()).toMatchObject({
@@ -13,13 +32,17 @@ describe('AnypayService', () => {
     act(() => {
       anypay.result.current.configure({
         description: 'Anypay demo invoice',
-        outputs: [{ satoshis: 1000, script: '11122223333' }],
       })
     })
     expect(anypay.result.current.getState()).toMatchObject({
       isLoading: false,
       description: 'Anypay demo invoice',
-      outputs: [{ satoshis: 1000, script: '11122223333' }],
     })
+  })
+
+  test('AnypayService#setupTransaction', () => {
+    anypay.result.current.setupTransaction({ outputs, inputs, changeTo })
+    anypay.result.current.buildTransaction({ keyPair })
+    console.log(anypay.result.current.getTransaction())
   })
 })
--- src/services/Anypay/index.ts
+++ src/services/Anypay/index.ts
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Forge } from 'txforge'
 
 export type IAnypayService = {
 
@@ -7,12 +8,14 @@ export type IAnypayService = {
 export type IAnypayServiceResponse = {
   configure: (args : IAnypayServiceConfigure) => void
   getState: () => IAnypayServiceGetStateResponse
+  setupTransaction: (args : IAnypayServiceSetupTransaction) => void
+  buildTransaction: (args : IAnypayServiceBuildTransaction) => void
+  getTransaction: () => string
 }
 
 export type IAnypayServiceGetStateResponse = {
   isLoading: boolean
   description: string
-  outputs: IAnypayServiceConfigureOutputs[]
 }
 
 export type IAnypayServiceConfigureOutputs = {
@@ -20,9 +23,41 @@ export type IAnypayServiceConfigureOutputs = {
   script: string
 }
 
+export type IAnypayServiceConfigureInputs = {
+  address: string
+  txid: string
+  vout: number
+  amount: number
+  satoshis: number
+  value: number
+  height: number
+  confirmations: number
+  scriptPubKey: string
+  script: string
+  outputIndex: number
+}
+
+export type IAnypayServiceSetupTransaction = {
+  inputs: IAnypayServiceConfigureInputs[]
+  outputs: IAnypayServiceConfigureOutputs[]
+  changeTo: string
+}
+
+export type IAnypayServiceBuildTransaction = {
+  keyPair: any
+}
+
 export type IAnypayServiceConfigure = {
   description: string
-  outputs: IAnypayServiceConfigureOutputs[]
+}
+
+export type IAnypayServiceConfigureResponse = {
+  forge: {
+    tx: any
+    inputs: any
+    outputs: any
+    options: any
+  }
 }
 
 const AnypayService = () : IAnypayServiceResponse => {
@@ -31,6 +66,8 @@ const AnypayService = () : IAnypayServiceResponse => {
     description: '',
     outputs: [],
   })
+
+  const forge = new Forge()
 
   /**
    * Get service state
@@ -42,20 +79,37 @@ const AnypayService = () : IAnypayServiceResponse => {
   /**
    * 
    */
-  const setup = () => {
-    
+  const setupTransaction = ({ inputs, outputs, changeTo } : IAnypayServiceSetupTransaction) => {
+    forge.addInput(inputs)
+    forge.addOutput(outputs)
+    forge.changeTo = changeTo
   }
+
+  /**
+   * 
+   */
+  const buildTransaction = ({ keyPair } : IAnypayServiceBuildTransaction) => {
+    forge.build().sign({ keyPair })
+  }
+
+  /**
+   * 
+   */
+  const getTransaction = () => forge.tx.toHex()
 
   /**
    * Configure and Initialize the service
    */
-  const configure = ({ description, outputs } : IAnypayServiceConfigure) => {
-    setState(() => ({ isLoading: false, description, outputs }))
+  const configure = ({ description } : IAnypayServiceConfigure) => {
+    setState(() => ({ isLoading: false, description }))
   }
 
   return ({
     getState,
     configure,
+    setupTransaction,
+    buildTransaction,
+    getTransaction,
   })
 }
 
